023d28177b1c3965182030974e07def5517f9312
arid0910 <arid0910@g.educaand.es>
2023-12-01 09:42:41 +0100

    1 constructor añadido mas getters, setter y to string

--- src/main/java/daw/Naipes.java
+++ src/main/java/daw/Naipes.java
@@ -4,13 +4,53 @@
 
 package daw;
 
+import java.util.Random;
+
 /**
  *
  * @author adam
  */
 public class Naipes {
     
+    Random random = new Random();
+    
     private int numCartas;
     private String palo; 
-   
+    
+    int[] arrayNumCartas = {1,2,3,4,5,6,7,8,9,10};
+    String[] arrayPalo = {"Oros","Bastos","Espadas","Copas"};
+    
+    public Naipes() {
+        this.numCartas = arrayNumCartas[random.nextInt(10)];
+        this.palo = arrayPalo[random.nextInt(4)];
+    }
+
+    public int getNumCartas() {
+        return numCartas;
+    }
+
+    public void setNumCartas(int numCartas) {
+        this.numCartas = numCartas;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Naipes{");
+        sb.append("numCartas=").append(numCartas);
+        sb.append(", palo=").append(palo);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
 }
