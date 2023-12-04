80268d7c96d15aaf3ffde878bee66e798b1d12bd
arid0910 <arid0910@g.educaand.es>
2023-12-04 08:28:46 +0100

    2 metodo añadido

--- src/main/java/daw/Baraja.java
+++ src/main/java/daw/Baraja.java
@@ -4,6 +4,8 @@
  */
 package daw;
 
+import java.util.Scanner;
+
 /**
  *
  * @author adam
@@ -11,14 +13,39 @@ package daw;
 public class Baraja {
 
     public static String cartaAleatoria(Naipes n) {
-        
+
         int numCarta = n.getNumCartas();
         String palo = n.getPalo();
         String resu = """
                       Tu carta es:
                        - Nº carta ->%s
                        - Palo ->%s
-                      """.formatted(numCarta,palo);
+                      """.formatted(numCarta, palo);
+        return resu;
+    }
+
+    public static String cartaAElegir(Naipes n) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un nº de carta");
+        int numCarta = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Introduce un palo");
+        String palo = sc.nextLine();
+
+        int numCartaAle;
+        String paloAle;
+        String resu;
+        do {
+            numCartaAle = n.getNumCartas();
+            paloAle = n.getPalo();
+            resu = """
+                      Tu carta es:
+                       - Nº carta ->%s
+                       - Palo ->%s
+                      """.formatted(numCartaAle, paloAle);
+        } while (numCartaAle == numCarta && paloAle.equalsIgnoreCase(palo));
+
         return resu;
     }
 }
